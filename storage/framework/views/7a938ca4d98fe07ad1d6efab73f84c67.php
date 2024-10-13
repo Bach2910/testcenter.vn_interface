13914da341af0abbdbc6a37dbd1a237200ed6b47
Phạm Ngọc Bách <ngoc22003@gmail.com>
2024-10-13 11:41:18 +0700

    Delete storage directory

--- storage/framework/views/7a938ca4d98fe07ad1d6efab73f84c67.php
+++
@@ -1,9 +0,0 @@
-<div class="carousel-item active">
-    <img src="<?php echo e($img); ?>"
-         class="d-block w-100" alt="Image 1">
-    <div class="carousel-caption ">
-        <a href="/blog/tai-lieu/" type="button" class="btn btn-danger"><?php echo e($button_title); ?></a>
-        <h5><?php echo e($name); ?></h5>
-    </div>
-</div>
-<?php /**PATH C:\laragon\www\interface\resources\views/components/news_item.blade.php ENDPATH**/ ?>
